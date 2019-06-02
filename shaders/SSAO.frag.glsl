5a98d140cc3538b19a3597d2939dfb296caf2559
Timur Gafarov <gecko0307@gmail.com>
2019-06-03 00:33:41 +0300

    Removed SSAO_MOD3

--- shaders/SSAO.frag.glsl
+++ shaders/SSAO.frag.glsl
@@ -35,11 +35,10 @@ uniform float ssaoPower;
 
 #define SSAO_SCALE 1.0
 #define SSAO_BIAS 0.05
-#define SSAO_MOD3 vec3(0.1031, 0.11369, 0.13787)
 
 float hash(vec2 p)
 {
-    vec3 p3 = fract(vec3(p.xyx) * SSAO_MOD3);
+    vec3 p3 = fract(vec3(p.xyx) * vec3(0.1031, 0.11369, 0.13787));
     p3 += dot(p3, p3.yzx + 19.19);
     return fract((p3.x + p3.y) * p3.z);
 }
